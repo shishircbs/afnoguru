b1344dc3ea89ad25ca1afa196003fb1e50e01270
shishircbs <shishir.cbs@gmail.com>
2023-01-16 01:25:23 +0545

    given the connection

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function connect(){
+    //mongoose.set('strictQuery', false)
     await mongoose.connect(process.env.ATLAS_URI)
     console.log("Database Connected")
 }
